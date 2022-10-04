76b30b20d4966cc708f8f1a879342698b1ee1fdb
Nathiya Ayyadurai <102350008+nathiyaayyatdurai@users.noreply.github.com>
2022-10-04 11:09:33 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,5 @@
 const mongodb = require('mongodb')
-const dbName = 'zendesk'
-const dbUrl = `mongodb+srv://Raj2710:Raj2710@raj.x3e0h.mongodb.net/${dbName}?retryWrites=true&w=majority`
+const dbName = 'nathiya'
+const dbUrl = "mongodb+srv://nathiya:Nathi123@cluster0.fjrgjvk.mongodb.net/?retryWrites=true&w=majority"
 
 module.exports ={mongodb,dbName,dbUrl}
